8669241f256d9775fbca5b9fb33a2a753f8f9da4
garyghayrat <gary.ghayrat@pm.me>
2024-10-08 11:49:57 -0400

    Remove natspec

--- src/interfaces/IEarningPowerCalculator.sol
+++ src/interfaces/IEarningPowerCalculator.sol
@@ -4,24 +4,11 @@ pragma solidity ^0.8.23;
 /// @notice Interface for calculating earning power of a staker based on their delegate score in
 /// GovernanceStaker.
 interface IEarningPowerCalculator {
-  /// @notice Calculates the earning power for a given delegate and staking amount.
-  /// @param _amountStaked The amount of tokens staked.
-  /// @param _staker The address of the staker.
-  /// @param _delegatee The address of the delegatee.
-  /// @return _earningPower The calculated earning power.
   function getEarningPower(uint256 _amountStaked, address _staker, address _delegatee)
     external
     view
     returns (uint256 _earningPower);
 
-  /// @notice Calculates the new earning power and determines if it qualifies for an update.`
-  /// @param _amountStaked The amount of tokens staked.
-  /// @param _staker The address of the staker.
-  /// @param _delegatee The address of the delegatee.
-  /// @param _oldEarningPower The previous earning power value.
-  /// @return _newEarningPower The newly calculated earning power.
-  /// @return _isQualifiedForUpdate Boolean indicating if the new earning power qualifies for an
-  /// update.
   function getNewEarningPower(
     uint256 _amountStaked,
     address _staker,
