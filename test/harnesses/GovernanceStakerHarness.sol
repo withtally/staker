a750e2955a5f5721929ec7b41c6e14c385747df4
garyghayrat <gary.ghayrat@pm.me>
2024-10-30 13:11:53 -0400

    Move `permitAndStake` and `permitAndStakeMore` into an extension contract

--- test/harnesses/GovernanceStakerHarness.sol
+++ test/harnesses/GovernanceStakerHarness.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.23;
 
 import {DelegationSurrogateVotes} from "src/DelegationSurrogateVotes.sol";
 import {GovernanceStaker} from "src/GovernanceStaker.sol";
-
+import {GovernanceStakerPermitAndStake} from "src/extensions/GovernanceStakerPermitAndStake.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import {EIP712} from "openzeppelin/utils/cryptography/EIP712.sol";
@@ -11,7 +11,7 @@ import {IERC20Delegates} from "src/interfaces/IERC20Delegates.sol";
 import {IEarningPowerCalculator} from "src/interfaces/IEarningPowerCalculator.sol";
 import {DelegationSurrogate} from "src/DelegationSurrogate.sol";
 
-contract GovernanceStakerHarness is GovernanceStaker {
+contract GovernanceStakerHarness is GovernanceStaker, GovernanceStakerPermitAndStake {
   constructor(
     IERC20 _rewardsToken,
     IERC20Delegates _stakeToken,
