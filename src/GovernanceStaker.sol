ea6f82fe71944b8b703fa183a001eb1ea9df017d
garyghayrat <gary.ghayrat@pm.me>
2024-10-30 13:11:53 -0400

    Move `permitAndStake` and `permitAndStakeMore` into an extension contract

--- src/GovernanceStaker.sol
+++ src/GovernanceStaker.sol
@@ -343,32 +343,6 @@ abstract contract GovernanceStaker is INotifiableRewardReceiver, Multicall, EIP7
     _depositId = _stake(msg.sender, _amount, _delegatee, _beneficiary);
   }
 
-  /// @notice Method to stake tokens to a new deposit. Before the staking operation occurs, a
-  /// signature is passed to the token contract's permit method to spend the would-be staked amount
-  /// of the token.
-  /// @param _amount Quantity of the staking token to stake.
-  /// @param _delegatee Address to assign the governance voting weight of the staked tokens.
-  /// @param _beneficiary Address that will accrue rewards for this stake.
-  /// @param _deadline The timestamp after which the permit signature should expire.
-  /// @param _v ECDSA signature component: Parity of the `y` coordinate of point `R`
-  /// @param _r ECDSA signature component: x-coordinate of `R`
-  /// @param _s ECDSA signature component: `s` value of the signature
-  /// @return _depositId Unique identifier for this deposit.
-  /// @dev Neither the delegatee nor the beneficiary may be the zero address. The deposit will be
-  /// owned by the message sender.
-  function permitAndStake(
-    uint256 _amount,
-    address _delegatee,
-    address _beneficiary,
-    uint256 _deadline,
-    uint8 _v,
-    bytes32 _r,
-    bytes32 _s
-  ) external virtual returns (DepositIdentifier _depositId) {
-    try STAKE_TOKEN.permit(msg.sender, address(this), _amount, _deadline, _v, _r, _s) {} catch {}
-    _depositId = _stake(msg.sender, _amount, _delegatee, _beneficiary);
-  }
-
   /// @notice Stake tokens to a new deposit on behalf of a user, using a signature to validate the
   /// user's intent. The caller must pre-approve the staking contract to spend at least the
   /// would-be staked amount of the token.
@@ -418,32 +392,6 @@ abstract contract GovernanceStaker is INotifiableRewardReceiver, Multicall, EIP7
   function stakeMore(DepositIdentifier _depositId, uint256 _amount) external virtual {
     Deposit storage deposit = deposits[_depositId];
     _revertIfNotDepositOwner(deposit, msg.sender);
-    _stakeMore(deposit, _depositId, _amount);
-  }
-
-  /// @notice Add more staking tokens to an existing deposit. A staker should call this method when
-  /// they have an existing deposit, and wish to stake more while retaining the same delegatee and
-  /// beneficiary. Before the staking operation occurs, a signature is passed to the token
-  /// contract's permit method to spend the would-be staked amount of the token.
-  /// @param _depositId Unique identifier of the deposit to which stake will be added.
-  /// @param _amount Quantity of stake to be added.
-  /// @param _deadline The timestamp after which the permit signature should expire.
-  /// @param _v ECDSA signature component: Parity of the `y` coordinate of point `R`
-  /// @param _r ECDSA signature component: x-coordinate of `R`
-  /// @param _s ECDSA signature component: `s` value of the signature
-  /// @dev The message sender must be the owner of the deposit.
-  function permitAndStakeMore(
-    DepositIdentifier _depositId,
-    uint256 _amount,
-    uint256 _deadline,
-    uint8 _v,
-    bytes32 _r,
-    bytes32 _s
-  ) external virtual {
-    Deposit storage deposit = deposits[_depositId];
-    _revertIfNotDepositOwner(deposit, msg.sender);
-
-    try STAKE_TOKEN.permit(msg.sender, address(this), _amount, _deadline, _v, _r, _s) {} catch {}
     _stakeMore(deposit, _depositId, _amount);
   }
 
