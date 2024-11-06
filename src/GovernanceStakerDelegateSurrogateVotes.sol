59ca1ec941c7428178d3ec611a517d2bb16353f6
keating <keating.dev@protonmail.com>
2024-11-06 09:28:12 -0500

    Move surrogates

--- src/GovernanceStakerDelegateSurrogateVotes.sol
+++ src/GovernanceStakerDelegateSurrogateVotes.sol
@@ -9,6 +9,14 @@ abstract contract GovernanceStakerDelegateSurrogateVotes is GovernanceStaker {
   /// @notice Emitted when a surrogate contract is deployed.
   event SurrogateDeployed(address indexed delegatee, address indexed surrogate);
 
+  /// @notice Maps the account of each governance delegate with the surrogate contract which holds
+  /// the staked tokens from deposits which assign voting weight to said delegate.
+  mapping(address delegatee => DelegationSurrogate surrogate) private storedSurrogates;
+
+  function surrogates(address _delegatee) public view override returns (DelegationSurrogate) {
+    return storedSurrogates[_delegatee];
+  }
+
   /// @notice Internal method which finds the existing surrogate contract—or deploys a new one if
   /// none exists—for a given delegatee.
   /// @param _delegatee Account for which a surrogate is sought.
@@ -19,11 +27,11 @@ abstract contract GovernanceStakerDelegateSurrogateVotes is GovernanceStaker {
     override
     returns (DelegationSurrogate _surrogate)
   {
-    _surrogate = surrogates[_delegatee];
+    _surrogate = storedSurrogates[_delegatee];
 
     if (address(_surrogate) == address(0)) {
       _surrogate = new DelegationSurrogateVotes(STAKE_TOKEN, _delegatee);
-      surrogates[_delegatee] = _surrogate;
+      storedSurrogates[_delegatee] = _surrogate;
       emit SurrogateDeployed(_delegatee, address(_surrogate));
     }
   }
