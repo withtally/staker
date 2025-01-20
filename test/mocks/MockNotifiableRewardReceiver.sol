51c66fe2eeee87ab7dcb5b76524a9052aa73d034
Ben DiFrancesco <ben@scopelift.co>
2025-01-20 14:00:04 -0500

    Implement and test a `transferFrom` based reward notifier
    Create and test a reward notifier contract that uses `transferFrom` to pull reward tokens
    from a pre-approved source address on a regular cadence. The source, reward amount, and
    reward interval are configurable by the contract's owner. It is assumed the source has
    used `approve` to allow the notifier contract to pull the tokens. The notify method is open
    and can be called by anyone. It does not provide a tip to the caller: the assumption is that
    enough stakers will be incentivized such that someone will call it. DAOs can also set up a
    bot do this automatically.

---
+++ test/mocks/MockNotifiableRewardReceiver.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.23;
+
+import {INotifiableRewardReceiver, IERC20} from "src/interfaces/INotifiableRewardReceiver.sol";
+
+contract MockNotifiableRewardReceiver is INotifiableRewardReceiver {
+  IERC20 public immutable REWARD_TOKEN;
+
+  uint256 public lastParam_notifyRewardAmount_amount;
+
+  constructor(IERC20 _rewardToken) {
+    REWARD_TOKEN = _rewardToken;
+  }
+
+  function notifyRewardAmount(uint256 _amount) external {
+    lastParam_notifyRewardAmount_amount = _amount;
+  }
+}
