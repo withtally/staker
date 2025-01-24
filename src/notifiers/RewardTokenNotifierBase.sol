ebf360bd4c3e1fe4dac5f3d9d6d365de76bcbb4f
Ben DiFrancesco <ben@scopelift.co>
2025-01-23 23:48:50 -0500

    Write natspec for the reard notifier contracts

--- src/notifiers/RewardTokenNotifierBase.sol
+++ src/notifiers/RewardTokenNotifierBase.sol
@@ -4,20 +4,64 @@ pragma solidity ^0.8.23;
 import {INotifiableRewardReceiver, IERC20} from "src/interfaces/INotifiableRewardReceiver.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 
+/// @title RewardTokenNotifierBase
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice An abstract reward notifier contract for managing the distribution of rewards to Staker
+/// instances.
+///
+/// Specifically, this base contract manages the details of directly distributing tokens
+/// of the same type as the reward token. In other words, value does not need to be converted in
+/// any way from one type of token to the reward token, rather reward tokens are simply moved to
+/// Staker instance.
+///
+/// The contract is not opinionated about how the tokens are "moved" to the notifier. Inheriting
+/// contracts must implement the `_sendTokensToReceiver` method to effectuate the movement of the
+/// reward tokens using whatever mechanic is desired. The implementation must ensure that exactly
+/// rewardAmount tokens are moved to the receiver.
+///
+/// The contract enforces a minimum interval between reward notifications. The contract ensures
+/// that a set amount of reward tokens are distributed each time. These parameters are updatable by
+/// the contract owner, which will presumably be the DAO itself in most instances.
 abstract contract RewardTokenNotifierBase is Ownable {
+  /// @notice Emitted when the reward amount is changed.
+  /// @param oldRewardAmount The previous reward amount.
+  /// @param newRewardAmount The new reward amount.
   event RewardAmountSet(uint256 oldRewardAmount, uint256 newRewardAmount);
+
+  /// @notice Emitted when the reward interval is changed.
+  /// @param oldRewardAmount The previous reward interval duration.
+  /// @param newRewardAmount The new reward interval duration.
   event RewardIntervalSet(uint256 oldRewardAmount, uint256 newRewardAmount);
+
+  /// @notice Emitted when rewards are distributed to the receiver.
+  /// @param rewardAmount The amount of rewards that were distributed.
+  /// @param nextRewardTime The timestamp after which the next reward can be distributed.
   event Notified(uint256 rewardAmount, uint256 nextRewardTime);
 
+  /// @notice Thrown if a caller attempts to notify rewards before the reward interval has elapsed.
   error RewardTokenNotifierBase__RewardIntervalNotElapsed();
 
+  /// @notice The contract that will receive reward notifications. Typically an instance of Staker.
   INotifiableRewardReceiver public immutable RECEIVER;
+
+  /// @notice The ERC20 token in which rewards are denominated.
   IERC20 public immutable TOKEN;
 
+  /// @notice The amount of reward tokens to be distributed in each notification.
   uint256 public rewardAmount;
+
+  /// @notice The minimum time that must elapse between reward notifications.
   uint256 public rewardInterval;
+
+  /// @notice The timestamp after which the next reward notification can be sent.
   uint256 public nextRewardTime = 0;
 
+  /// @param _receiver The contract that will receive reward notifications. Typically an instance
+  /// of Staker.
+  /// @param _initialRewardAmount The initial amount of reward tokens to be distributed per
+  /// notification.
+  /// @param _initialRewardInterval The initial minimum time that must elapse between
+  /// notifications.
   constructor(
     INotifiableRewardReceiver _receiver,
     uint256 _initialRewardAmount,
@@ -29,6 +73,11 @@ abstract contract RewardTokenNotifierBase is Ownable {
     _setRewardInterval(_initialRewardInterval);
   }
 
+  /// @notice Notifies the receiver contract of a new reward, sending the reward tokens and
+  /// triggering the distribution of those tokens to stakers.
+  /// @dev The reward interval must have elapsed since the last notification.
+  /// @dev The tokens are "sent" to the receiver contract using whatever mechanism is implemented
+  /// in the `_sendTokensToReceiver` method.
   function notify() external virtual {
     if (block.timestamp < nextRewardTime) {
       revert RewardTokenNotifierBase__RewardIntervalNotElapsed();
@@ -40,25 +89,39 @@ abstract contract RewardTokenNotifierBase is Ownable {
     emit Notified(rewardAmount, nextRewardTime);
   }
 
+  /// @notice Sets a new reward amount to be used in future notifications.
+  /// @param _newRewardAmount The new amount of reward tokens to distribute per notification.
+  /// @dev Caller must be the contract owner.
   function setRewardAmount(uint256 _newRewardAmount) external virtual {
     _checkOwner();
     _setRewardAmount(_newRewardAmount);
   }
 
+  /// @notice Sets a new reward interval to be used between future notifications.
+  /// @param _newRewardInterval The new minimum time that must elapse between notifications.
+  /// @dev Caller must be the contract owner.
   function setRewardInterval(uint256 _newRewardInterval) external virtual {
     _checkOwner();
     _setRewardInterval(_newRewardInterval);
   }
 
+  /// @notice Internal helper method which sets a new reward amount.
+  /// @param _newRewardAmount The new amount of reward tokens to distribute per notification.
   function _setRewardAmount(uint256 _newRewardAmount) internal {
     emit RewardAmountSet(rewardAmount, _newRewardAmount);
     rewardAmount = _newRewardAmount;
   }
 
+  /// @notice Internal helper method which sets a new reward interval.
+  /// @param _newRewardInterval The new minimum time that must elapse between notifications.
   function _setRewardInterval(uint256 _newRewardInterval) internal {
     emit RewardIntervalSet(rewardInterval, _newRewardInterval);
     rewardInterval = _newRewardInterval;
   }
 
+  /// @notice Internal abstract method which must be implemented by inheritors to send tokens
+  /// to the receiver contract.
+  /// @dev This method will be called before the receiver is notified of new rewards. The
+  /// implementation must ensure that exactly rewardAmount tokens are moved to the receiver.
   function _sendTokensToReceiver() internal virtual;
 }
