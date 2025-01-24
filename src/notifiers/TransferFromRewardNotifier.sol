e5c1c71f469ac602e3ce8680668ed070863ef3f1
Ben DiFrancesco <ben@scopelift.co>
2025-01-23 23:48:50 -0500

    Write natspec for the reard notifier contracts

--- src/notifiers/TransferFromRewardNotifier.sol
+++ src/notifiers/TransferFromRewardNotifier.sol
@@ -6,13 +6,35 @@ import {RewardTokenNotifierBase} from "src/notifiers/RewardTokenNotifierBase.sol
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
+/// @title TransferFromRewardNotifier
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice A reward notifier that uses ERC20 transferFrom to move reward tokens from a designated
+/// source address to the Staker contract. This implementation is suitable when reward tokens are
+/// held by a separate address (like a treasury) that has approved this contract to spend tokens
+/// on its behalf.
+///
+/// The contract allows the owner to configure both the reward parameters (amount and interval) as
+/// well as the source address from which rewards will be transferred. The source address must
+/// approve this contract to spend reward tokens before rewards can be distributed.
 contract TransferFromRewardNotifier is RewardTokenNotifierBase {
   using SafeERC20 for IERC20;
 
+  /// @notice Emitted when the reward source address is changed.
+  /// @param oldRewardSource The previous address expected to provide reward tokens.
+  /// @param newRewardSource The new address expected to provide reward tokens.
   event RewardSourceSet(address oldRewardSource, address newRewardSource);
 
+  /// @notice The address from which reward tokens will be transferred when distributing rewards.
   address public rewardSource;
 
+  /// @param _receiver The contract that will receive reward notifications, typically an instance
+  /// of Staker.
+  /// @param _initialRewardAmount The initial amount of reward tokens to be distributed per
+  /// notification.
+  /// @param _initialRewardInterval The initial minimum time that must elapse between
+  /// notifications.
+  /// @param _initialOwner The address that will have permission to update contract parameters.
+  /// @param _initialRewardSource The initial address from which rewards will be transferred.
   constructor(
     INotifiableRewardReceiver _receiver,
     uint256 _initialRewardAmount,
@@ -26,16 +48,26 @@ contract TransferFromRewardNotifier is RewardTokenNotifierBase {
     _setRewardSource(_initialRewardSource);
   }
 
+  /// @notice Sets a new address from which reward tokens will be transferred.
+  /// @param _newRewardSource The new address that will provide reward tokens.
+  /// @dev Caller must be the contract owner. The new source address must approve this contract
+  /// to spend reward tokens before rewards can be distributed.
   function setRewardSource(address _newRewardSource) external {
     _checkOwner();
     _setRewardSource(_newRewardSource);
   }
 
+  /// @notice Internal helper method which sets a new reward source address.
+  /// @param _newRewardSource The new address that will provide reward tokens.
   function _setRewardSource(address _newRewardSource) internal {
     emit RewardSourceSet(rewardSource, _newRewardSource);
     rewardSource = _newRewardSource;
   }
 
+  /// @inheritdoc RewardTokenNotifierBase
+  /// @dev Transfers exactly rewardAmount tokens from the rewardSource address to the receiver
+  /// using transferFrom. The rewardSource must have approved this contract to spend at least
+  /// rewardAmount tokens.
   function _sendTokensToReceiver() internal virtual override {
     TOKEN.transferFrom(rewardSource, address(RECEIVER), rewardAmount);
   }
--- src/notifiers/TransferRewardNotifier.sol
+++ src/notifiers/TransferRewardNotifier.sol
@@ -6,9 +6,24 @@ import {RewardTokenNotifierBase} from "src/notifiers/RewardTokenNotifierBase.sol
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
+/// @title TransferRewardNotifier
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice A reward notifier that uses ERC20 transfer to move reward tokens from this contract's
+/// balance to the Staker contract. This implementation is suitable when reward tokens are held
+/// directly by the notifier contract itself, rather than being transferred from a separate source.
+///
+/// The contract allows the owner to configure the reward parameters (amount and interval). The
+/// contract must maintain a sufficient balance of reward tokens for distributions to succeed.
 contract TransferRewardNotifier is RewardTokenNotifierBase {
   using SafeERC20 for IERC20;
 
+  /// @param _receiver The contract that will receive reward notifications, typically an instance
+  /// of Staker.
+  /// @param _initialRewardAmount The initial amount of reward tokens to be distributed per
+  /// notification.
+  /// @param _initialRewardInterval The initial minimum time that must elapse between
+  /// notifications.
+  /// @param _initialOwner The address that will have permission to update contract parameters.
   constructor(
     INotifiableRewardReceiver _receiver,
     uint256 _initialRewardAmount,
@@ -19,6 +34,10 @@ contract TransferRewardNotifier is RewardTokenNotifierBase {
     Ownable(_initialOwner)
   {}
 
+  /// @inheritdoc RewardTokenNotifierBase
+  /// @dev Transfers exactly rewardAmount tokens from this contract's balance to the receiver
+  /// using transfer. This contract must have a sufficient balance of reward tokens for the
+  /// transfer to succeed.
   function _sendTokensToReceiver() internal virtual override {
     TOKEN.transfer(address(RECEIVER), rewardAmount);
   }
