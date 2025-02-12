2d1ba30e0e7ff87a715791f88b82ef47bc7010e5
garyghayrat <gary.ghayrat@pm.me>
2025-02-12 16:09:10 -0500

    Test sum of depositor earning power equals totalEarningPower

--- test/Staker.invariants.t.sol
+++ test/Staker.invariants.t.sol
@@ -63,6 +63,10 @@ contract StakerInvariants is Test {
     assertEq(govStaker.totalStaked(), handler.reduceDelegates(0, this.accumulateSurrogateBalance));
   }
 
+  function invariant_Sum_of_depositor_earning_power_equals_total_earning_power() public {
+    assertEq(govStaker.totalEarningPower(), handler.reduceDepositors(0, this.accumulateEarningPower));
+  }
+
   function invariant_Cumulative_staked_minus_withdrawals_equals_total_stake() public view {
     assertEq(govStaker.totalStaked(), handler.ghost_stakeSum() - handler.ghost_stakeWithdrawn());
   }
@@ -94,6 +98,10 @@ contract StakerInvariants is Test {
 
   function accumulateDeposits(uint256 balance, address depositor) external view returns (uint256) {
     return balance + govStaker.depositorTotalStaked(depositor);
+  }
+
+  function accumulateEarningPower(uint256 earningPower, address depositor) external view returns (uint256) {
+    return earningPower + govStaker.depositorTotalEarningPower(depositor);
   }
 
   function accumulateSurrogateBalance(uint256 balance, address delegate)
