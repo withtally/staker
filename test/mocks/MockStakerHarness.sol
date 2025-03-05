d1c95649cde8d79d3c4222fa07003a1382d6ee2c
garyghayrat <gary.ghayrat@pm.me>
2025-03-05 05:46:29 -0500

    Update import paths

--- test/mocks/MockStakerHarness.sol
+++ test/mocks/MockStakerHarness.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.23;
 
-import {Staker} from "src/Staker.sol";
-import {StakerPermitAndStake} from "src/extensions/StakerPermitAndStake.sol";
-import {StakerDelegateSurrogateVotes} from "src/extensions/StakerDelegateSurrogateVotes.sol";
+import {Staker} from "../../src/Staker.sol";
+import {StakerPermitAndStake} from "../../src/extensions/StakerPermitAndStake.sol";
+import {StakerDelegateSurrogateVotes} from "../../src/extensions/StakerDelegateSurrogateVotes.sol";
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC20Staking} from "src/interfaces/IERC20Staking.sol";
-import {IEarningPowerCalculator} from "src/interfaces/IEarningPowerCalculator.sol";
-import {DelegationSurrogate} from "src/DelegationSurrogate.sol";
+import {IERC20Staking} from "../../src/interfaces/IERC20Staking.sol";
+import {IEarningPowerCalculator} from "../../src/interfaces/IEarningPowerCalculator.sol";
+import {DelegationSurrogate} from "../../src/DelegationSurrogate.sol";
 
 /// @dev Mock version of StakerHarness that accepts different stake tokens for each inherited
 /// contract, unlike StakerHarness which uses the same token. This contract is used to test reverts
