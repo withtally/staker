9a13ea6ce68c0a738e6592ffc0f1c9dab02e6629
keating <keating.dev@protonmail.com>
2025-03-12 21:09:29 -0400

    Cleanup a little

--- test/DeployBase.t.sol
+++ test/DeployBase.t.sol
@@ -1,17 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.23;
 
-import {Vm, Test, stdStorage, StdStorage, console2, stdError} from "forge-std/Test.sol";
 import {Test} from "forge-std/Test.sol";
 import {IEarningPowerCalculator} from "../src/interfaces/IEarningPowerCalculator.sol";
 import {MintRewardNotifier} from "../src/notifiers/MintRewardNotifier.sol";
-import {DeployBase} from "../src/script/DeployBase.sol";
 import {Staker} from "../src/Staker.sol";
 import {DeployBaseHarness} from "./harnesses/DeployBaseHarness.sol";
 import {ERC20Fake} from "./fakes/ERC20Fake.sol";
 import {ERC20VotesMock} from "./mocks/MockERC20Votes.sol";
 
-// Setup harness
 contract DeployBaseTest is Test {
   ERC20Fake rewardToken;
   ERC20VotesMock govToken;
@@ -27,9 +24,6 @@ contract DeployBaseTest is Test {
     deployScript = new DeployBaseHarness(rewardToken, govToken);
   }
 }
-
-// test run, mock configuration that is fuzzed
-// check byte code matches
 
 contract Run is DeployBaseTest {
   function test_StakingSystemDeploy() public {
@@ -49,16 +43,4 @@ contract Run is DeployBaseTest {
     assertEq(address(govToken), address(_staker.STAKE_TOKEN()));
     assertEq(address(deployScript.admin()), _staker.admin());
   }
-
-  function testfuzz_StakingSystemDeployWithMockStakerConfiguration() public {
-    //
-    (IEarningPowerCalculator _calculator, Staker _staker, address[] memory _notifiers) =
-      deployScript.run();
-    console2.logUint(_notifiers.length);
-    assertEq(address(_calculator), address(_staker.earningPowerCalculator()));
-    assertTrue(_staker.isRewardNotifier(_notifiers[0]));
-    assertEq(address(rewardToken), address(_staker.REWARD_TOKEN()));
-    assertEq(address(govToken), address(_staker.STAKE_TOKEN()));
-    assertEq(address(deployScript.admin()), _staker.admin());
-  }
-}
+ }
