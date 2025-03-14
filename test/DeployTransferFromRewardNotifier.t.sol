e2a119c1d7ac0d294539403441d65721e41976e2
funkyenough <14842981+funkyenough@users.noreply.github.com>
2025-03-14 21:27:37 +0900

    feat: add deploy script for TransferFromRewardNotifier

---
+++ test/DeployTransferFromRewardNotifier.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+import {Staker} from "../src/Staker.sol";
+import {IEarningPowerCalculator} from "../src/interfaces/IEarningPowerCalculator.sol";
+import {INotifiableRewardReceiver} from "../src/interfaces/INotifiableRewardReceiver.sol";
+import {TransferFromRewardNotifier} from "../src/notifiers/TransferFromRewardNotifier.sol";
+import {DeployTransferFromRewardNotifierHarness} from
+  "./harnesses/DeployTransferFromRewardNotifierHarness.sol";
+import {ERC20Fake} from "./fakes/ERC20Fake.sol";
+import {ERC20VotesMock} from "./mocks/MockERC20Votes.sol";
+
+contract DeployTransferFromRewardNotifierTest is Test {
+  ERC20Fake rewardToken;
+  ERC20VotesMock govToken;
+  DeployTransferFromRewardNotifierHarness deployScript;
+
+  function setUp() public {
+    rewardToken = new ERC20Fake();
+    vm.label(address(rewardToken), "Reward Token");
+
+    govToken = new ERC20VotesMock();
+    vm.label(address(govToken), "Governance Token");
+
+    deployScript = new DeployTransferFromRewardNotifierHarness(rewardToken, govToken);
+  }
+}
+
+contract run is DeployTransferFromRewardNotifierTest {
+  function test_DeployTransferFromRewardNotifier() public {
+    (, Staker _staker, address[] memory _notifiers) = deployScript.run();
+
+    TransferFromRewardNotifier _transferFromNotifier = TransferFromRewardNotifier(_notifiers[0]);
+    assertEq(address(_staker), address(_transferFromNotifier.RECEIVER()));
+    assertEq(10e18, _transferFromNotifier.rewardAmount());
+    assertEq(30 days, _transferFromNotifier.rewardInterval());
+    assertEq(deployScript.notifierOwner(), _transferFromNotifier.owner());
+    assertEq(address(deployScript.notifierRewardSource()), address(_transferFromNotifier.rewardSource()));
+  }
+}
