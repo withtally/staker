dcf0e5a4c31a5825a2e46efcdc587d4fc931ed33
funkyenough <14842981+funkyenough@users.noreply.github.com>
2025-03-21 22:45:37 +0900

    add test for set reward source

--- test/DeployTransferFromRewardNotifier.t.sol
+++ test/DeployTransferFromRewardNotifier.t.sol
@@ -8,6 +8,7 @@ import {DeployTransferFromRewardNotifierFake} from
   "./fakes/DeployTransferFromRewardNotifierFake.sol";
 import {ERC20Fake} from "./fakes/ERC20Fake.sol";
 import {ERC20VotesMock} from "./mocks/MockERC20Votes.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DeployTransferFromRewardNotifierTest is Test {
   ERC20Fake rewardToken;
@@ -37,5 +38,41 @@ contract run is DeployTransferFromRewardNotifierTest {
     assertEq(
       address(deployScript.notifierRewardSource()), address(_transferFromNotifier.rewardSource())
     );
+  }
+}
+
+contract SetRewardSource is DeployTransferFromRewardNotifierTest {
+  function testFuzz_UpdatesTheRewardSource(address _newRewardSource) public {
+    (,, address[] memory _notifiers) = deployScript.run();
+    TransferFromRewardNotifier _transferFromNotifier = TransferFromRewardNotifier(_notifiers[0]);
+    address owner = deployScript.notifierOwner();
+
+    vm.prank(owner);
+    _transferFromNotifier.setRewardSource(_newRewardSource);
+
+    assertEq(_transferFromNotifier.rewardSource(), _newRewardSource);
+  }
+
+  function testFuzz_EmitsAnEventForSettingTheRewardSource(address _newRewardSource) public {
+    (,, address[] memory _notifiers) = deployScript.run();
+    TransferFromRewardNotifier _transferFromNotifier = TransferFromRewardNotifier(_notifiers[0]);
+    address owner = deployScript.notifierOwner();
+    address _oldRewardSource = deployScript.notifierRewardSource();
+
+    vm.expectEmit();
+    emit TransferFromRewardNotifier.RewardSourceSet(_oldRewardSource, _newRewardSource);
+    vm.prank(owner);
+    _transferFromNotifier.setRewardSource(_newRewardSource);
+  }
+
+  function testFuzz_RevertIf_CallerIsNotOwner(address _newRewardSource, address _notOwner) public {
+    (,, address[] memory _notifiers) = deployScript.run();
+    TransferFromRewardNotifier _transferFromNotifier = TransferFromRewardNotifier(_notifiers[0]);
+    address owner = deployScript.notifierOwner();
+
+    vm.assume(_notOwner != owner);
+    vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, _notOwner));
+    vm.prank(_notOwner);
+    _transferFromNotifier.setRewardSource(_newRewardSource);
   }
 }
