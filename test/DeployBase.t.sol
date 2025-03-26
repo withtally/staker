5358005a4aeaf42d704e4eaedc2c2594bf53c569
keating <keating.dev@protonmail.com>
2025-03-26 09:58:27 -0400

    Add multiple reward notifiers

--- test/DeployBase.t.sol
+++ test/DeployBase.t.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.23;
 import {Test} from "forge-std/Test.sol";
 import {IEarningPowerCalculator} from "../src/interfaces/IEarningPowerCalculator.sol";
 import {MintRewardNotifier} from "../src/notifiers/MintRewardNotifier.sol";
+import {TransferRewardNotifier} from "../src/notifiers/TransferRewardNotifier.sol";
 import {Staker} from "../src/Staker.sol";
 import {DeployBaseFake} from "./fakes/DeployBaseFake.sol";
+import {DeployMultipleRewardNotifiersFake} from "./fakes/DeployMultipleRewardNotifiersFake.sol";
 import {ERC20Fake} from "./fakes/ERC20Fake.sol";
 import {ERC20VotesMock} from "./mocks/MockERC20Votes.sol";
 
@@ -42,5 +44,32 @@ contract Run is DeployBaseTest {
     assertEq(address(rewardToken), address(_staker.REWARD_TOKEN()));
     assertEq(address(govToken), address(_staker.STAKE_TOKEN()));
     assertEq(address(deployScript.admin()), _staker.admin());
+  }
+
+  function test_StakingSystemMultipleRewardNotifiersDeploy() public {
+    DeployMultipleRewardNotifiersFake _deployScript = new DeployMultipleRewardNotifiersFake(rewardToken, govToken);
+    (IEarningPowerCalculator _calculator, Staker _staker, address[] memory _notifiers) =
+      _deployScript.run();
+    MintRewardNotifier _mintNotifier = MintRewardNotifier(_notifiers[0]);
+    assertEq(address(_staker), address(_mintNotifier.RECEIVER()));
+    assertEq(10e18, _mintNotifier.rewardAmount());
+    assertEq(30 days, _mintNotifier.rewardInterval());
+    assertEq(deployScript.notifierOwner(), _mintNotifier.owner());
+    assertEq(address(deployScript.notifierMinter()), address(_mintNotifier.minter()));
+
+    TransferRewardNotifier _transferNotifier = TransferRewardNotifier(_notifiers[0]);
+    assertEq(address(_staker), address(_transferNotifier.RECEIVER()));
+    assertEq(10e18, _transferNotifier.rewardAmount());
+    assertEq(30 days, _transferNotifier.rewardInterval());
+    assertEq(deployScript.notifierOwner(), _transferNotifier.owner());
+
+    // Staker params
+    assertTrue(_staker.isRewardNotifier(_notifiers[0]));
+    assertTrue(_staker.isRewardNotifier(_notifiers[1]));
+    assertEq(address(_calculator), address(_staker.earningPowerCalculator()));
+    assertEq(address(rewardToken), address(_staker.REWARD_TOKEN()));
+    assertEq(address(govToken), address(_staker.STAKE_TOKEN()));
+    assertEq(address(deployScript.admin()), _staker.admin());
+
   }
 }
