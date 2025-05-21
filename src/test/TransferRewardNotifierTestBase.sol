253aca1a850fcefaad7451daccfd4bfc18e72089
funkyenough <14842981+funkyenough@users.noreply.github.com>
2025-05-21 20:20:49 +0900

    add TransferRewardNotifierTestBase and related test contracts

---
+++ src/test/TransferRewardNotifierTestBase.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// slither-disable-start reentrancy-benign
+
+pragma solidity ^0.8.23;
+
+import {TransferRewardNotifier} from "../notifiers/TransferRewardNotifier.sol";
+import {IMintable} from "src/interfaces/IMintable.sol";
+import {StakerTestBase} from "./StakerTestBase.sol";
+
+/// @notice Base contract for testing TransferRewardNotifier functionality. @dev Extends
+/// StakerTestBase and simulates calling notify function on TransferRewardNotifier. This contract is
+/// designed to be used in conjunction with the deployment scripts in
+/// `src/script/notifiers/DeployTransferRewardNotifier.sol`.
+abstract contract TransferRewardNotifierTestBase is StakerTestBase {
+  TransferRewardNotifier transferRewardNotifier;
+
+  function _notifyRewardAmount(uint256 _amount) public override {
+    address _owner = transferRewardNotifier.owner();
+
+    vm.prank(_owner);
+    transferRewardNotifier.setRewardAmount(_amount);
+
+    IMintable(address(REWARD_TOKEN)).mint(address(transferRewardNotifier), _amount);
+    transferRewardNotifier.notify();
+  }
+}
